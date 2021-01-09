ffad5f6107559ebb6bd41a410f3d73f37fe7f3d5
danaremar <danaremar@alum.us.es>
2021-01-09 20:58:24 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -84,7 +84,6 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 ini_set('display_errors','Off');
 ini_set('error_reporting', E_ALL );
-define('WP_DEBUG', false);
 define('WP_DEBUG_DISPLAY', false);
 
 /* That's all, stop editing! Happy publishing. */
